69a50d78acd9ad0c9489d5dd1e0f953f161205dc
Evan Ackmann <evansbee@gmail.com>
2018-01-22 13:29:13 -0500

    working on the parser

--- test_tokenizer.py
+++ test_tokenizer.py
@@ -43,6 +43,8 @@ tokens = [
 	'NUMBER',
 	'LABEL_DEF',
 	'LABEL_REF',
+	'ANON_LABEL_DEF',
+	'ANON_LABEL_REF',
 	'COMMENT',
 	'PLUS',
 	'MINUS',
@@ -63,21 +65,28 @@ t_MINUS = r'\-'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
 t_COMMENT = r';.*'
-
+t_ANON_LABEL_REF = r'\@b|\@f|\@r'
 
 def t_LABEL_DEF(t):
-	r'[\.\:][a-zA-Z][a-zA-Z0-9_]+'
-	if t.value[0] == ':':
-		t.lexer.label_prefix = t.value[1:]
-		t.value = t.value[1:]
+	r'[a-zA-Z][a-zA-Z0-9_]{,9}:|\.[a-zA-Z][a-zA-Z0-9_]{,8}:'
+	t.lexer.placement = False
+	if t.value[0] != '.':
+		t.lexer.label_prefix = t.value[:-1]
+		t.value = t.value[:-1]
 	else:
-		t.value = t.lexer.label_prefix + t.value
+		t.value = t.lexer.label_prefix + t.value[:-1]
 	return t
 
+def t_ANON_LABEL_DEF(t):
+	r'\@\@:'
+	t.lexer.placement = False
+	t.value = t.value[:-1]
+	return t
 
 def t_LABEL_REF(t):
-	r'[a-zA-Z][a-zA-Z0-9_]+(\.[a-zA-Z][a-zA-Z0-9_]+)?'
+	r'[a-zA-Z][a-zA-Z0-9_]{,9}(\.[a-zA-Z][a-zA-Z0-9_]{,8})?|\.[a-zA-Z][a-zA-Z0-9_]{,8}'
 	if t.value in OPCODES:
+		t.lexer.placement = False
 		t.type = 'OPCODE'
 		t.value = t.value
 	elif t.value in REGISTERS:
@@ -86,22 +95,21 @@ def t_LABEL_REF(t):
 	elif t.value == 'db':
 		t.type = 'DATA'
 		t.value = t.value
+	elif t.value[0] == '.': #local label
+		t.value = t.lexer.label_prefix + t.value
 	return t
 
 def t_NUMBER(t):
-	r'[0-9a-fA-F]+h|\d+'
+	r'[0-9a-fA-F]+h|\d+|0x[0-9a-fA-F]+'
 	if t.value[-1] == 'h':
 		t.value = int(t.value[:-1],16)
 	else:
 		t.value = int(t.value)
-	return t
 
-def t_PLACEMENT(t):
-	r'@[0-9a-fA-F]+h|@\d+'
-	if t.value[-1] == 'h':
-		t.value = int(t.value[1:-1],16)
-	else:
-		t.value = int(t.value[1:])
+
+	if t.lexer.placement:
+		t.lexer.placement = False
+		t.type = 'PLACEMENT'	
 	return t
 
 def t_error(t):
@@ -111,24 +119,31 @@ def t_error(t):
 def t_newline(t):
 	r'\n+'
 	t.lexer.lineno += len(t.value)	
+	t.lexer.placement = True
 	
 t_ignore  = ' \t'	
 
 	
 data = r'''
-@0000h         :init  jmp    init.jmp_0003
-@0002h                halt   
+0000h         init:  jmp    init.jmp_0003
+0001h
+0002h                halt   
 						db 0234h
 						db '2'
 						db '321314'
-@0003h     .jmp_0003  out    'bad\n'
-@000Fh                out    ';'     ;helloasd asdasd 
-@0007h                out    r0    ; tricky comments have 'this stuff' in them
-@0090h 		jmp init.jmp_0003
+0003h     .jmp_0003:  out    'bad\n'
+000Fh                out    ';'     ;helloasd asdasd 
+0007h                out    r0    ; tricky comments have 'this stuff' in them
+0090h 		jmp .jmp_0003
+jmp init
+1000h     @@:   nop
+jmp @b
+jmp @r ;go back
+jmp @f
 '''
 
 lexer = lex.lex()
-
+lexer.placement = True
 	
 lexer.input(data)
 while True:
@@ -136,3 +151,53 @@ while True:
 	if not tok: 
 		break      # No more input
 	print(tok)
+
+
+
+class Program(object):
+	def __init__(self):
+		pass
+	def assemble(self):
+		pass
+	def pretty_print(self):
+		pass	
+
+class Line(object):
+	def __init__(self):
+		pass
+	def assemble(self):
+		pass
+	def pretty_print(self):
+		pass	
+
+class Comment(object):
+	def __init__(self):
+		pass
+	def assemble(self):
+		pass
+	def pretty_print(self):
+		pass	
+
+class Opcode(object):
+	def __init__(self):
+		pass
+	def assemble(self):
+		pass
+	def pretty_print(self):
+		pass	
+
+class Arg(object):
+	def __init__(self):
+		pass
+	def assemble(self):
+		pass
+	def pretty_print(self):
+		pass	
+
+class Label(object):
+	def __init__(self):
+		pass
+	def assemble(self):
+		pass
+	def pretty_print(self):
+		pass	
