a8b3be0f1ad41e4148e874bcd8b47df6c0506f8c
Evan Ackmann <evansbee@gmail.com>
2018-01-24 17:23:26 -0500

    makde benchmark work in test mode

--- vm/benchmark.py
+++ vm/benchmark.py
@@ -1,33 +1,45 @@
 ''' Benchmarks a file'''
 
 import time
-from .assembler import AssembleFile
-from .emulator import VirtualMachine
+from assembler import AssembleFile
+from emulator import VirtualMachine
+
+import sys
+
+def println(s):
+    sys.stdout.write(s)
+    sys.stdout.flush()
+    
+
+def Benchmark(filename, runtime = 10.0, batch = 100000):
+    spinner = ['-','\\','|','/']
+    spinner_idx = 0
 
 
-
-def Benchmark(filename, runtime = 10.0, batch = 10000):
     print("*" * 80)
-    print("** Benchmarking File:",filename)
-    print("** Assembling...)
-    vm = VirtualMachine()
-    print("** Assembling...)
-    program = AssembleFile(filename)
-    vm.LoadProgram(program)
-    print("** Running Benchmark...)
+    print("[+] Benchmarking File:",filename)
+    #vm = VirtualMachine()
+    println("[-] Assembling...")
+    #program = AssembleFile(filename)
+    println("\r[+] Assembling...\n")
+    #vm.LoadProgram(program)
+    println("[-] Running Benchmark...")
     start = time.time()
     while True:
-        vm.RunNCycles(batch)
-        vm.output_buffer = ''
+        println("\r[{}] Running Benchmark...".format(spinner[spinner_idx % 4]))
+        spinner_idx += 1
+        #vm.RunNCycles(batch)
+        #vm.output_buffer = ''
+        time.sleep(.1)
         elapsed = time.time() - start
         if elapsed > runtime:
             break
-    print("** Benchmark Complete...)
+    println("\r[+] Running Benchmark...\n")
     hz = vm.cycles / elapsed
     mhz = hz / 1000000.0
 
-    print("** Executed {} cycles in {:.2}s".format(vm.cycles,elapsed))
-    print("** Effective clock rate: {:.1} MHz".format(mhz))
+    print(" [+] Executed {} cycles in {:.2}s".format(vm.cycles,elapsed))
+    print(" [+] Effective clock rate: {:.1} MHz".format(mhz))
     print("*" * 80)
 
 
